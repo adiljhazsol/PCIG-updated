b3fce3288bbef95da9843bdb3626cd2b8741f267
adiljhazsol <adiljhazsol@gmail.com>
2026-02-02 23:59:37 +0500

    Allow FRONTEND_URL env var in CORS config

--- PCIG_Backend/config/cors.php
+++ PCIG_Backend/config/cors.php
@@ -20,6 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
         'http://localhost:3000',
         'http://localhost:5173',
         'http://localhost:8000',
